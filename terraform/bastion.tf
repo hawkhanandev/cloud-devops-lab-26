ab184687663fcf5d68773fbc83230df30990de0e
hawkhanandev <hawkhanandev@gmail.com>
2026-08-10 22:45:11 +0500

    Fixed SG and Bastion Key pair

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "bastion" {
   vpc_security_group_ids = [aws_security_group.bastion.id]
   associate_public_ip_address = true
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
-  key_name = aws_key_pair.general_key_pair.key_name
+  key_name = aws_key_pair.general_key_pair_1.key_name
 
   tags = {
     Name = "bastion-devops-lab"
