08d95823b2877a250d5da2e309ad81eee66a8922
hawkhanandev <hawkhanandev@gmail.com>
2026-08-10 11:33:37 +0500

    Dir Name Changed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,45 +1,45 @@
 variable "aws_region" {
   description = "AWS region"
-  type        = string
-  default     = "us-east-1"
+  type = string
+  default = "us-east-1"
 }
 
 variable "environment" {
   description = "cloud-devops-lab-26"
-  type        = string
-  default     = "dev"
+  type = string
+  default = "dev"
 }
 
 variable "vpc_cidr" {
   description = "CIDR for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
+  type = string
+  default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   description = "Public Subnet CIDR (bastion host)"
-  type        = string
-  default     = "10.0.1.0/24"
+  type = string
+  default = "10.0.1.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "private subnet CIDR (app server)"
-  type        = string
-  default     = "10.0.2.0/24"
+  type = string
+  default = "10.0.2.0/24"
 }
 
 variable "availability_zone" {
   description = "AZ for subnets"
-  type        = string
-  default     = "us-east-1a"
+  type = string
+  default = "us-east-1a"
 }
 
 variable "key_pair_name" {
   description = "devopslab_accessKeys.csv"
-  type        = string
+  type = string
 }
 
 variable "my_ip" {
   description = "72.255.28.18"
-  type        = string
+  type = string
 }
